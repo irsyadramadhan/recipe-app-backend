25199e18d9d8d12ae7ef9cdea8effeeb3f45a924
Irsyad <ramadhanirsyad@icloud.com>
2023-03-13 13:29:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 4000
+const mainRouter = require('./src/routes')
+const cors = require('cors')
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use('/', mainRouter)
+app.use('/img', express.static('./tmp'))
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
